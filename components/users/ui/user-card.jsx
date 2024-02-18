ba976721a0b7351f7d6efd6427790e18a0cecf6a
3dmatern <denismatern@gmail.com>
2024-02-18 15:58:23 +0300

    feat: install avatar shadcn

--- components/users/ui/user-card.jsx
+++ components/users/ui/user-card.jsx
@@ -1,5 +1,6 @@
-import Image from "next/image";
 import Link from "next/link";
+
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 export function UserCard({ user }) {
     return (
@@ -7,13 +8,11 @@ export function UserCard({ user }) {
             href="#"
             className="flex items-center gap-3 py-2 px-3 hover:bg-slate-100"
         >
-            <Image
-                src={user?.avatar}
-                alt="avatar"
-                width={40}
-                height={40}
-                className="rounded-full"
-            />
+            <Avatar>
+                <AvatarImage src={user?.avatar} />
+                <AvatarFallback className="text-[10px]">avatar</AvatarFallback>
+            </Avatar>
+
             <div className="flex flex-col text-start">
                 <span className="font-semibold">
                     {user?.firstname} {user?.lastname}
